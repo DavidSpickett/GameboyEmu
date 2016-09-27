96dfcc52d964b101223da9c173f4668959d2c342
David Spickett <spickettdavid@googlemail.com>
2016-09-27 20:16:09 +0100

    Initial Commit

---
+++ GameboyEmu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameboyEmu
+//
+//  Created by David Spickett on 27/09/2016.
+//  Copyright © 2016 David Spickett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
